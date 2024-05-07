679b35b4d422327b51d70dfdf374ff30a7f89e68
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-05-07 12:52:07 +0530

    changes

---
+++ src/pages/api/models/Seller.ts
@@ -0,0 +1,59 @@
+// UserModel.js
+import mongoose, { Schema } from 'mongoose';
+
+const sellerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    telephone_no: {
+        type: String,
+        required: false
+    },
+    mobile_no: {
+        type: String,
+        required: false
+    },
+    fax: {
+        type: String,
+        required: false
+    },
+    pan: {
+        type: String,
+        required: false
+    },
+    gstin: {
+        type: String,
+        required: false
+    },
+    state_code: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now
+    },
+    updatedDate: {
+        type: Date,
+        default: null
+    },
+    deletedDate: {
+        type: Date,
+        default: null
+    }
+}, {
+    timestamps: { createdAt: 'createdDate', updatedAt: 'updatedDate' }
+});
+
+export const Seller = mongoose.models.User || mongoose.model('Seller', sellerSchema);
+
+
---
+++ src/pages/api/seller.ts
@@ -0,0 +1,78 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectToMongoDB from './libs/mongodb';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Error } from 'mongoose';
+import { Error as MongooseError } from 'mongoose';
+import runMiddleware from './libs/runMiddleware';
+import Cors from 'cors';
+import { Seller } from './models/Seller';
+
+
+
+
+interface ApiResponse {
+  message?: string;
+  error?: string;
+  errorDetail?: any;
+  data?: any;
+}
+
+const cors = Cors({
+  // Only allow requests with GET, POST and OPTIONS
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+});
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
+
+  await connectToMongoDB();
+  await runMiddleware(req, res, cors);
+  //const user = verifyToken(req);
+
+  // if (!user) {
+  //   return res.status(401).json({ message: 'Unauthorized' });
+  // } else {
+
+  if (req.method === 'POST') {
+
+    switch (req.body.type) {
+      case 'CREATE':
+        try {
+
+          const user = await Seller.create({
+            name: req.body.name,
+            address: req.body.address,
+            telephone_no: req.body.telephone_no,
+            mobile_no: req.body.mobile_no,
+            fax: req.body.fax,
+            pan: req.body.pan,
+            gstin: req.body.gstin,
+            state_code: req.body.state_code,
+            email: req.body.email
+          });
+
+
+          res.status(201).json({ message: 'Seller have been successfully created.' });
+        } catch (error: any) {
+          res.status(500).json({ error: 'Internal Error', errorDetail: 'An unexpected error occurred' });
+        }
+
+        break;
+      case 'LIST':
+        try {
+          const dataList = await Seller.find({}).exec();
+          res.status(200).json({ data: dataList });
+        } catch (error: any) {
+
+          if (error instanceof MongooseError) {
+            return res.status(500).json({ error: 'Database Error', errorDetail: error });
+          }
+          res.status(500).json({ error: 'Internal Server Error' });
+        }
+        break;
+    }
+
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+}
