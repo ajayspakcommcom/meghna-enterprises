8b8e23d70fe0bca90c50314cb3c3f31b0003d451
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-05-16 16:22:37 +0530

    changes

--- src/pages/api/user-test.ts
+++ src/pages/api/user-test.ts
@@ -2,7 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import Cors from 'cors';
 import runMiddleware from './libs/runMiddleware';
 import connectToMongoDB from './libs/mongodb';
-import { User } from './models/User';
+import PDFDocument from 'pdfkit';
+import fs from 'fs';
+import path from 'path';
+
 
 
 const cors = Cors({
@@ -12,23 +15,36 @@ const cors = Cors({
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     // Only allow GET requests
+    await connectToMongoDB();
     await runMiddleware(req, res, cors);
 
-    if (req.method !== 'GET') {
-        return res.status(405).json({ message: 'Only GET requests allowed' });
-    }
+    if (req.method === 'POST') {
 
-    try {
-        // Connect to MongoDB
-        await connectToMongoDB();
+        const doc = new PDFDocument();
 
-        // Fetch all users from the database
-        const users = await User.find();
+        const filePath = path.join(process.cwd(), 'public/pdf', 'contract.pdf');
+        const stream = fs.createWriteStream(filePath);
 
-        // Send the list of users in the response
-        res.status(200).json(users);
-    } catch (error) {
-        console.error('Error while fetching users:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        doc.pipe(stream);
+
+        const textContent = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book`;
+
+        // doc.fontSize(30).fillColor('red').text('Hello, World!', 10, 15);
+        // doc.fontSize(14).fillColor('black').text(textContent, 10, 50);
+        // doc.fontSize(10).fillColor('brown').text('This is Done', 10, 200);
+
+        const imagePath = path.join(process.cwd(), 'public/pdf', 'logo.svg');
+        doc.image(imagePath, {
+            fit: [250, 250],
+            align: 'center',
+            valign: 'center'
+        });
+
+        doc.end();
+
+        res.status(201).json({ message: 'Pdf Generated created.' });
+
+    } else {
+        res.status(405).json({ error: 'Method Not Allowed' });
     }
 }
