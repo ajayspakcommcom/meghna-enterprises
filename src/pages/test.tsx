0f84094aa02e24b0c81957bde9f8e3894461d6bf
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-05-20 13:27:08 +0530

    changes

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -6,30 +6,15 @@ import dynamic from 'next/dynamic';
 const Header = dynamic(() => import('../../components/header/index'));
 
 
-
 export default function Index() {
 
-    const router = useRouter();
 
-
-    useEffect(() => {
-
-        const token = localStorage.getItem("token");
-        if (!token) {
-            router.push('/');
-        }
-
-    }, []);
-
-    const goToPage = (url: string) => {
-        router.push(`${url}`);
-    };
 
 
     return (
         <>
             <Header />
-            <h1>Test</h1>
+
 
         </>
     );
