5693a8f0937dfc35b9c8a2731aed93268673c29d
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-06-07 11:40:14 +0530

    changes

--- src/pages/api/generate-pdf.ts
+++ src/pages/api/generate-pdf.ts
@@ -20,6 +20,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     if (req.method === 'POST') {
 
+
+
         switch (req.body.type) {
             case 'EMAIL-SEND':
                 try {
@@ -191,7 +193,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                                         </div>
                                     `;
 
-                    await sendEmail({ recipient: 'ajay@spakcomm.com', subject: 'Feeds & Seeds Contract', text: htmlContent });
+                    await sendEmail({ recipient: 'ajay@spakcomm.com', subject: `Contract Copy (${req.body.contract_no})`, text: htmlContent });
 
                     res.status(200).json({ message: 'Pdf sent successfully.' });
                 } catch (error: any) {
