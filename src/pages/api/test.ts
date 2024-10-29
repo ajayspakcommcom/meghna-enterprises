f765dbfa231ba14be8c4edb36d69ddd92f0615d7
Ajay Spak <ajay@spakcomm.com>
2024-10-29 17:16:06 +0530

    changes

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,13 +1,13 @@
 const pdf = require('html-pdf-node');
 const fs = require('fs');
 
-const html = '<h1>Hello, World!</h1>';
+const html = '<h1>Hello, World!...</h1>';
 const file = { content: html };
 
 (async () => {
     try {
         const pdfBuffer = await pdf.generatePdf(file, { format: 'A4' });
-        fs.writeFileSync('generated.pdf', pdfBuffer);
+        fs.writeFileSync('billing.pdf', pdfBuffer);
         console.log('PDF generated successfully!');
     } catch (error) {
         console.error("Error generating PDF:", error);
